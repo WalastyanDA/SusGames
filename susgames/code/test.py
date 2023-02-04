2c14f9afc3730849d250a0ed8935274cda91bf83
Dom Binks <dominic.binks@gmail.com>
2023-02-04 11:30:00 +0000

    Add test.py

---
+++ susgames/code/test.py
@@ -0,0 +1,18 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode([800, 600]);
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    screen.fill((255,128,255))
+    pygame.draw.circle(screen, (0,0,255), (250,250), 75)
+
+    pygame.display.flip()
+
+pygame.quit()
